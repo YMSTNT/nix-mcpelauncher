984e88ab0c3d90e951d81d97b8de464762bde3d3
YMSTNT <21342713+YMSTNT@users.noreply.github.com>
2023-12-17 15:55:35 +0100

    Bump version

--- flake.nix
+++ flake.nix
@@ -9,8 +9,8 @@
     app = appimageTools.wrapType2 {
       name = "mcpelauncher";
     src = pkgs.fetchurl {
-      url = "https://github.com/minecraft-linux/appimage-builder/releases/download/v0.11.2-718/Minecraft_Bedrock_Launcher-x86_64-v0.11.2.718.AppImage";
-      sha256 = "049cl0x1xz1g33hls4qncpspvgdapfx3m5jd9dvzs0kcyipvamjs";
+      url = "https://github.com/minecraft-linux/appimage-builder/releases/download/v0.12.1-763/Minecraft_Bedrock_Launcher-x86_64-v0.12.1.763.AppImage";
+      sha256 = "sha256-FHC4+nzldBau0ViRmgdUk2nPIPJi8WI4IxQTtzpO1W4=";
     };
     extraPkgs = pkgs: with pkgs; [ ];
   };
